d6dbb3386dc6b6ddc894dc0382ef3f080e83c4b9
mahmoud <mahmoud.g.hussein194@gmail.com>
2020-08-17 17:16:03 +0300

    intilz commit

---
+++ app/src/main/java/com/example/swoosh/welcome_screen.kt
@@ -0,0 +1,11 @@
+package com.example.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class welcome_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_screen)
+    }
+}
