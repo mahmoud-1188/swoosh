010fc8a4224c90829e3481809b337f4a1cb4b4aa
mahmoud <mahmoud.g.hussein194@gmail.com>
2020-08-18 12:40:03 +0300

    lesson 1

---
+++ app/src/main/java/com/example/swoosh/legueoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class legueoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_legueo)
+    }
+}
